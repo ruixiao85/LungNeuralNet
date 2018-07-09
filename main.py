99ccb4e7100844f06fccf0bd5f81688297302df3
CarolS LabWin10 <ruixiao85@gmail.com>
2018-07-09 02:15:07 -0400

    Lung Inflammation. RGB Unet 5 Conv Relu Sigmoid.

---
+++ main.py
@@ -0,0 +1,100 @@
+import os
+import numpy as np
+from skimage.io import imsave, imread
+from model import dice_coef, dice_coef_loss, get_unet3, get_unet5, get_unet6
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+
+
+def standardize(preimg):
+    mean = np.mean(preimg)
+    std = np.std(preimg)
+    preimg -= mean
+    preimg /= (3. * std)
+    preimg = np.tanh(preimg)
+    return preimg
+
+def preprocess(preimg, gray, stand):
+    if (gray):
+        if (stand):
+            preimg = standardize(preimg)
+        return preimg[..., np.newaxis]
+    else:
+        preimg = preimg / 255.
+        if (stand):
+            preimg = standardize(preimg)
+        return preimg
+
+
+WorkDir = os.getcwd()
+ImageOrig = "ori"
+ImageMask = "msk"
+ImageTest = "test"
+ImagePred = "pred"
+image_cols = 696
+image_rows = 520
+ifGray = False
+nChannel = 1 if ifGray else 3
+weightFile = 'weights.h5'
+
+
+def train_and_predict(contTrain=True):
+    print("Scanning subfolders [/%s] and [/%s] of [%s]" % (ImageOrig, ImageMask, WorkDir))
+    images = os.listdir(os.path.join(WorkDir, ImageOrig))
+    masks = os.listdir(os.path.join(WorkDir, ImageMask))
+    print("%d images and %d masks found" % (len(images), len(masks)))
+
+    images = list(set(images).intersection(masks))  # image-mask pairs only
+    total_train = len(images)
+    print("%d image-mask pairs accepted" % len(images))
+
+    img = np.ndarray((total_train, image_rows, image_cols, nChannel), dtype=np.float32)
+    msk = np.ndarray((total_train, image_rows, image_cols, 1), dtype=np.float32)
+
+    for i, image_name in enumerate(images):
+        img[i] = preprocess(imread(os.path.join(WorkDir, ImageOrig, image_name), as_grey=ifGray), ifGray, True)
+        # imsave("testimg.jpg", img[i])
+        msk[i] = preprocess(imread(os.path.join(WorkDir, ImageMask, image_name), as_grey=True), True, False)
+        # imsave("testmsk.jpg", msk[i])
+        print('Done: {0}/{1} images'.format(i, total_train))
+
+    # model = get_unet3(image_rows, image_cols, nChannel)
+    model = get_unet5(image_rows, image_cols, nChannel)
+    # model = get_unet6(image_rows, image_cols, nChannel)
+
+    if contTrain and os.path.exists(weightFile):
+        model.load_weights(weightFile)
+    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+
+    print('Creating model and checkpoint...')
+    model_checkpoint = ModelCheckpoint(weightFile, monitor='val_loss', save_best_only=True)
+
+    print('Fitting model...')
+    model.fit(img, msk, batch_size=4, nb_epoch=12, verbose=1, shuffle=True,
+              validation_split=0.2, callbacks=[model_checkpoint])
+
+    print('Loading and preprocessing test data...')
+    print("Scanning test subfolders [/%s] of [%s]" % (ImageTest, WorkDir))
+    testimages = os.listdir(os.path.join(WorkDir, ImageTest))
+
+    tst = np.ndarray((len(testimages), image_rows, image_cols, nChannel), dtype=np.float32)
+    total_test = len(tst)
+
+    for i, image_name in enumerate(testimages):
+        tst[i] = preprocess(imread(os.path.join(WorkDir, ImageTest, image_name), as_grey=ifGray), ifGray, True)
+        print('Done: {0}/{1} images'.format(i, total_test))
+
+    print('Predicting masks on test data...')
+    # model.load_weights(weightFile)
+    imgs_mask_test = model.predict(tst, verbose=1)
+
+    print('Saving predicted masks to files...')
+    if not os.path.exists(ImagePred):
+        os.mkdir(ImagePred)
+    for i, image in enumerate(imgs_mask_test):
+        image = (image[:, :, 0] * 255.).astype(np.uint8)
+        imsave(os.path.join(ImagePred, testimages[i] + '_pred.png'), image)
+
+
+if __name__ == '__main__':
+    train_and_predict(True)
---
+++ model.py
@@ -0,0 +1,202 @@
+from __future__ import print_function
+
+import os
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, UpSampling2D, Cropping2D, \
+    ZeroPadding2D
+from keras import backend as KerasBackend
+
+KerasBackend.set_image_data_format('channels_last')
+Smooth = 0.1
+num_class = 1
+concat_axis = 3
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = KerasBackend.flatten(y_true)
+    y_pred_f = KerasBackend.flatten(y_pred)
+    intersection = KerasBackend.sum(y_true_f * y_pred_f)
+    return (2. * intersection + Smooth) / (KerasBackend.sum(y_true_f) + KerasBackend.sum(y_pred_f) + Smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+
+def get_crop_shape(target, refer):
+    # width, the 3rd dimension
+    cw = (target.get_shape()[2] - refer.get_shape()[2]).value
+    assert (cw >= 0)
+    if cw % 2 != 0:
+        cw1, cw2 = int(cw / 2), int(cw / 2) + 1
+    else:
+        cw1, cw2 = int(cw / 2), int(cw / 2)
+    # height, the 2nd dimension
+    ch = (target.get_shape()[1] - refer.get_shape()[1]).value
+    assert (ch >= 0)
+    if ch % 2 != 0:
+        ch1, ch2 = int(ch / 2), int(ch / 2) + 1
+    else:
+        ch1, ch2 = int(ch / 2), int(ch / 2)
+    return (ch1, ch2), (cw1, cw2)
+
+
+def get_unet3(img_rows, img_cols, dim=1):
+    inputs = Input((img_rows, img_cols, dim))
+
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv1_1')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+
+    up_conv2 = UpSampling2D(size=(2, 2))(conv3)
+    ch, cw = get_crop_shape(conv2, up_conv2)
+    crop_conv2 = Cropping2D(cropping=(ch, cw))(conv2)
+    up2 = concatenate([up_conv2, crop_conv2], axis=concat_axis)
+    decon2 = Conv2D(64, (3, 3), activation='relu', padding='same')(up2)
+    decon2 = Conv2D(64, (3, 3), activation='relu', padding='same')(decon2)
+
+    up_conv1 = UpSampling2D(size=(2, 2))(decon2)
+    ch, cw = get_crop_shape(conv1, up_conv1)
+    crop_conv1 = Cropping2D(cropping=(ch, cw))(conv1)
+    up1 = concatenate([up_conv1, crop_conv1], axis=concat_axis)
+    decon1 = Conv2D(32, (3, 3), activation='relu', padding='same')(up1)
+    decon1 = Conv2D(32, (3, 3), activation='relu', padding='same')(decon1)
+
+    ch, cw = get_crop_shape(inputs, decon1)
+    decon1 = ZeroPadding2D(padding=((ch[0], ch[1]), (cw[0], cw[1])))(decon1)
+    outputs = Conv2D(num_class, (1, 1), activation='sigmoid')(decon1)
+
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def get_unet5(img_rows, img_cols, dim=1):
+    inputs = Input((img_rows, img_cols, dim))
+
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv1_1')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+    up_conv4 = UpSampling2D(size=(2, 2))(conv5)
+    ch, cw = get_crop_shape(conv4, up_conv4)
+    crop_conv4 = Cropping2D(cropping=(ch, cw))(conv4)
+    up4 = concatenate([up_conv4, crop_conv4], axis=concat_axis)
+    decon4 = Conv2D(256, (3, 3), activation='relu', padding='same')(up4)
+    decon4 = Conv2D(256, (3, 3), activation='relu', padding='same')(decon4)
+
+    up_conv3 = UpSampling2D(size=(2, 2))(decon4)
+    ch, cw = get_crop_shape(conv3, up_conv3)
+    crop_conv3 = Cropping2D(cropping=(ch, cw))(conv3)
+    up3 = concatenate([up_conv3, crop_conv3], axis=concat_axis)
+    decon3 = Conv2D(128, (3, 3), activation='relu', padding='same')(up3)
+    decon3 = Conv2D(128, (3, 3), activation='relu', padding='same')(decon3)
+
+    up_conv2 = UpSampling2D(size=(2, 2))(decon3)
+    ch, cw = get_crop_shape(conv2, up_conv2)
+    crop_conv2 = Cropping2D(cropping=(ch, cw))(conv2)
+    up2 = concatenate([up_conv2, crop_conv2], axis=concat_axis)
+    decon2 = Conv2D(64, (3, 3), activation='relu', padding='same')(up2)
+    decon2 = Conv2D(64, (3, 3), activation='relu', padding='same')(decon2)
+
+    up_conv1 = UpSampling2D(size=(2, 2))(decon2)
+    ch, cw = get_crop_shape(conv1, up_conv1)
+    crop_conv1 = Cropping2D(cropping=(ch, cw))(conv1)
+    up1 = concatenate([up_conv1, crop_conv1], axis=concat_axis)
+    decon1 = Conv2D(32, (3, 3), activation='relu', padding='same')(up1)
+    decon1 = Conv2D(32, (3, 3), activation='relu', padding='same')(decon1)
+
+    ch, cw = get_crop_shape(inputs, decon1)
+    decon1 = ZeroPadding2D(padding=((ch[0], ch[1]), (cw[0], cw[1])))(decon1)
+    outputs = Conv2D(num_class, (1, 1), activation='sigmoid')(decon1)
+
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def get_unet6(img_rows, img_cols, dim=1):
+    inputs = Input((img_rows, img_cols, dim))
+
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv1_1')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+    pool5 = MaxPooling2D(pool_size=(2, 2))(conv5)
+
+    conv6 = Conv2D(1024, (3, 3), activation='relu', padding='same')(pool5)
+    conv6 = Conv2D(1024, (3, 3), activation='relu', padding='same')(conv6)
+
+    up_conv5 = UpSampling2D(size=(2, 2))(conv6)
+    ch, cw = get_crop_shape(conv5, up_conv5)
+    crop_conv5 = Cropping2D(cropping=(ch, cw))(conv5)
+    up5 = concatenate([up_conv5, crop_conv5], axis=concat_axis)
+    decon5 = Conv2D(512, (3, 3), activation='relu', padding='same')(up5)
+    decon5 = Conv2D(512, (3, 3), activation='relu', padding='same')(decon5)
+
+    up_conv4 = UpSampling2D(size=(2, 2))(decon5)
+    ch, cw = get_crop_shape(conv4, up_conv4)
+    crop_conv4 = Cropping2D(cropping=(ch, cw))(conv4)
+    up4 = concatenate([up_conv4, crop_conv4], axis=concat_axis)
+    decon4 = Conv2D(256, (3, 3), activation='relu', padding='same')(up4)
+    decon4 = Conv2D(256, (3, 3), activation='relu', padding='same')(decon4)
+
+    up_conv3 = UpSampling2D(size=(2, 2))(decon4)
+    ch, cw = get_crop_shape(conv3, up_conv3)
+    crop_conv3 = Cropping2D(cropping=(ch, cw))(conv3)
+    up3 = concatenate([up_conv3, crop_conv3], axis=concat_axis)
+    decon3 = Conv2D(128, (3, 3), activation='relu', padding='same')(up3)
+    decon3 = Conv2D(128, (3, 3), activation='relu', padding='same')(decon3)
+
+    up_conv2 = UpSampling2D(size=(2, 2))(decon3)
+    ch, cw = get_crop_shape(conv2, up_conv2)
+    crop_conv2 = Cropping2D(cropping=(ch, cw))(conv2)
+    up2 = concatenate([up_conv2, crop_conv2], axis=concat_axis)
+    decon2 = Conv2D(64, (3, 3), activation='relu', padding='same')(up2)
+    decon2 = Conv2D(64, (3, 3), activation='relu', padding='same')(decon2)
+
+    up_conv1 = UpSampling2D(size=(2, 2))(decon2)
+    ch, cw = get_crop_shape(conv1, up_conv1)
+    crop_conv1 = Cropping2D(cropping=(ch, cw))(conv1)
+    up1 = concatenate([up_conv1, crop_conv1], axis=concat_axis)
+    decon1 = Conv2D(32, (3, 3), activation='relu', padding='same')(up1)
+    decon1 = Conv2D(32, (3, 3), activation='relu', padding='same')(decon1)
+
+    ch, cw = get_crop_shape(inputs, decon1)
+    decon1 = ZeroPadding2D(padding=((ch[0], ch[1]), (cw[0], cw[1])))(decon1)
+    outputs = Conv2D(num_class, (1, 1), activation='sigmoid')(decon1)
+
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
