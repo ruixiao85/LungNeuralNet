851ee3518ccde14abdf6792025f9e87fb25b9d7e
CarolS LabWin10 <ruixiao85@gmail.com>
2018-10-25 17:58:08 -0400

    +fluorescent intensity per channel script

---
+++ main_sum_intensity_per_channel.py
@@ -0,0 +1,19 @@
+import argparse
+from util import find_file_recursive
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='train and predict with biomedical images.')
+    parser.add_argument('-d', '--dir', dest='dir', action='store',
+                        default='D:/Cel files/2018-10.25 Uchenna Green Blue', help='work directory')
+    parser.add_argument('-e', '--ext', dest='ext', action='store',
+                        default='*.tif', help='extension')
+    args = parser.parse_args()
+
+    print("filename,blue_sum,green_sum,red_sum")
+    for img in find_file_recursive(args.dir,args.ext):
+        im=cv2.imread(img)
+        # print(im.dtype)
+        # print(im.shape)
+        print(img+','+','.join(str(n) for n in np.sum(im,axis=(0,1)).tolist()))
