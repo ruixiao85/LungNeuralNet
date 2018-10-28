50337ab3402e0f25a7d101c7c7cfa33f332ea5d7
CarolSuper <ruixiao85@gmail.com>
2018-10-27 21:09:50 -0400

    install if not find. text to column. write to csv.

--- ggplot2.R
+++ ggplot2.R
@@ -1,25 +1,29 @@
 args <- commandArgs(trailingOnly = TRUE)
 wd <- ifelse(
    length(args) < 1,
-   "D:/Cel files/2018-07.08 Keras Tensorflow 2X IMAGES/LungNeuralNet/10x_scan_lung_smoke",
+   "./10x_scan_lung_smoke",
    is.na(args[1])
 )
+
+list.of.packages <- c("ggplot2", "data.table","reshape")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
 library(ggplot2)
 setwd(wd)
 files <- dir(pattern = ".csv")
 # filter <- "1.0_512"
 # filter <- "softmax"
-filter <- "unet1"
-config<-'cat_resize_dim_net_nout_filt_downconv_downsamp_upconv_upsamp_actf_outf_lossf'
+filter <- "UNet1a"
+cfg<-'cat_resize_dim_net_filt_down_up_func'
 df_list = NULL
 for (f in files) {
    if (grepl(filter, f)) {
-      # print(f)
+      print(f)
       df <- read.csv(f)
-      df[,config] <- f
+      df[,cfg] <- f
       df_list[[f]] <- df
-      # print(df[1:3,])
+      print(df[1:3,])
    }
 }
 print(length(df_list))
@@ -30,8 +34,8 @@ head(df_all <- rbindlist(df_list))
 df <- as.data.frame(df_all)
 
 require(reshape)
-df <- transform(df, config = colsplit(df[,config], split = "_",names = strsplit(config,"_")[[1]]))
+df <- transform(df, config = colsplit(df[,cfg], split = "_", names = strsplit(cfg,"_")[[1]]))
 # df <- data.frame(do.call('rbind', strsplit(as.character(df[,config]),'_',fixed=TRUE)))
-# write.csv(df,paste0("all",filter,".csv"))
-
-
+writefile=paste0("all",filter,".csv")
+write.csv(df,writefile)
+print(writefile)
