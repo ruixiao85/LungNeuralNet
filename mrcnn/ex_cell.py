230507e2e4a090da0cf3d871188ad4af0d346a99
CarolS LabWin10 <ruixiao85@gmail.com>
2018-11-01 18:39:53 -0400

    -RLE +csv<-header,result

--- mrcnn/ex_cell.py
+++ mrcnn/ex_cell.py
@@ -44,7 +44,13 @@ from mrcnn import visualize
 # a variety of images to serve as a validation set.
 VAL_IMAGE_IDS = [
 '37_3-WT-WT-SM_001',
-'37_3-WT-WT-SM_001---Copy'
+'37_3-WT-WT-SM_001---Copy',
+'42_4-WT-ZZ-RA_001 - Copy',
+'42_4-WT-ZZ-RA_001 - Copy - Copy',
+'42_4-WT-ZZ-RA_001---Copy',
+'42_4-WT-ZZ-RA_001---Copy - Copy',
+'43_1 KO ZZ RA_002 - Copy',
+'43_1 KO ZZ RA_002 - Copy - Copy'
 
 # '024435_2017-09-07 13_54_54_#5003#9025#2420#3188#635#1403#',
 # '024435_2017-09-07 13_54_54_#5003#9025#2420#3188#6352#7120#',
@@ -302,67 +308,6 @@ def train(model,subset_dir,args):
                 augmentation=augmentation,
                 layers='all')
 
-
-############################################################
-#  RLE Encoding
-############################################################
-
-def rle_encode(mask):
-    """Encodes a mask in Run Length Encoding (RLE).
-    Returns a string of space-separated values.
-    """
-    assert mask.ndim == 2, "Mask must be of shape [Height, Width]"
-    # Flatten it column wise
-    m = mask.T.flatten()
-    # Compute gradient. Equals 1 or -1 at transition points
-    g = np.diff(np.concatenate([[0], m, [0]]), n=1)
-    # 1-based indicies of transition points (where gradient != 0)
-    rle = np.where(g != 0)[0].reshape([-1, 2]) + 1
-    # Convert second index in each pair to lenth
-    rle[:, 1] = rle[:, 1] - rle[:, 0]
-    return " ".join(map(str, rle.flatten()))
-
-
-def rle_decode(rle, shape):
-    """Decodes an RLE encoded list of space separated
-    numbers and returns a binary mask."""
-    rle = list(map(int, rle.split()))
-    rle = np.array(rle, dtype=np.int32).reshape([-1, 2])
-    rle[:, 1] += rle[:, 0]
-    rle -= 1
-    mask = np.zeros([shape[0] * shape[1]], np.bool)
-    for s, e in rle:
-        assert 0 <= s < mask.shape[0]
-        assert 1 <= e <= mask.shape[0], "shape: {}  s {}  e {}".format(shape, s, e)
-        mask[s:e] = 1
-    # Reshape and transpose
-    mask = mask.reshape([shape[1], shape[0]]).T
-    return mask
-
-
-def mask_to_rle(image_id, mask, scores):
-    "Encodes instance masks to submission format."
-    assert mask.ndim == 3, "Mask must be [H, W, count]"
-    # If mask is empty, return line with image ID only
-    if mask.shape[-1] == 0:
-        return "{},".format(image_id)
-    # Remove mask overlaps
-    # Multiply each instance mask by its score order
-    # then take the maximum across the last dimension
-    order = np.argsort(scores)[::-1] + 1  # 1-based descending
-    mask = np.max(mask * np.reshape(order, [1, 1, -1]), -1)
-    # Loop over instance masks
-    lines = []
-    for o in order:
-        m = np.where(mask == o, 1, 0)
-        # Skip if empty
-        if m.sum() == 0.0:
-            continue
-        rle = rle_encode(m)
-        lines.append("{}, {}".format(image_id, rle))
-    return "\n".join(lines)
-
-
 ############################################################
 #  Detection
 ############################################################
@@ -386,35 +331,31 @@ def detect(model,subset_dir,args):
     dataset.prepare()
     # Load over images
     submission = []
+    header='file,lym,mono,pmn' # dataset.class_names[1],dataset.class_names[2],dataset.class_names[3]
+    submission.append(header); print(header)
+    import collections
     for image_id in dataset.image_ids:
-    # for image_id in
-        # Load image and run detection
         image = dataset.load_image(image_id)
-        # Detect objects
         r = model.detect([image], verbose=0)[0]
-        # Encode image to RLE. Returns a string of multiple lines
         source_id = dataset.image_info[image_id]["id"]
-        rle = mask_to_rle(source_id, r["masks"], r["scores"])
-        submission.append(rle)
+        countmap=collections.Counter(r['class_ids'])
+        result=','.join([source_id,str(countmap.get(1,0)),str(countmap.get(2,0)),str(countmap.get(3,0))])
+        submission.append(result); print(result)
         # Save image with masks
         visualize.display_instances(
-            image, r['rois'], r['masks'], r['class_ids'],
-            dataset.class_names, r['scores'],
-            show_bbox=False, show_mask=False,
-            title="Predictions")
-        plt.savefig("{}/{}.png".format(submit_dir, dataset.image_info[image_id]["id"]))
+                image, r['rois'], r['masks'], r['class_ids'],
+                dataset.class_names, r['scores'],
+                show_bbox=False, show_mask=False,
+                title=' '.join(["Predictions",header,result]))
+        plt.savefig("{}/{}.png".format(submit_dir, source_id))
 
     # Save to csv file
-    submission = "ImageId,EncodedPixels\n" + "\n".join(submission)
+    submission = "ImageId,\n" + "\n".join(submission)
     file_path = os.path.join(submit_dir, "submit.csv")
     with open(file_path, "w") as f:
         f.write(submission)
     print("Saved to ", submit_dir)
 
-
-############################################################
-#  Command Line
-############################################################
 
 def move_file_structure(ext=".jpg"):
     for filename in os.listdir(subset_dir):
@@ -425,6 +366,10 @@ def move_file_structure(ext=".jpg"):
             os.makedirs(os.path.join(subset_dir,filenoext,'images'))
             os.rename(os.path.join(subset_dir,filename),os.path.join(subset_dir,filenoext,'images',filename))
 
+
+############################################################
+#  Command Line
+############################################################
 
 if __name__ == '__main__':
     import argparse
