02aa4e260b34fd2f857e1520b52d5bb47d2a108e
CarolSuper <ruixiao85@gmail.com>
2018-11-12 19:03:21 -0500

    +augmentation for ImagePathSet

--- preprocess.py
+++ preprocess.py
@@ -207,10 +207,19 @@ def augment_image_pair(_img, _tgt, _level):
         seq_det = seg_both_3.to_deterministic()
         return seq_img_6.augment_images(seq_det.augment_images(_img)), seq_det.augment_images(_tgt)
 
+image_set_aug = iaa.SomeOf((0, 2), [
+    iaa.Fliplr(0.5),
+    iaa.Flipud(0.5),
+    iaa.OneOf([iaa.Affine(rotate=90),
+               iaa.Affine(rotate=180),
+               iaa.Affine(rotate=270)]),
+    iaa.Multiply((0.8, 1.5)),
+    iaa.GaussianBlur(sigma=(0.0, 5.0))
+])
 def augment_image_set(_img, _msk, _level, _tgt_ch=1):
     if _level<1:
         return _img, _msk
-    seq_det = seg_both_1.to_deterministic() # if 1<=_level<2 else seg_both_2.to_deterministic()
+    seq_det = image_set_aug.to_deterministic() # if 1<=_level<2 else seg_both_2.to_deterministic()
     _img=seq_det.augment_image(_img)
     for i in range(_msk.shape[-1]):
         _msk[...,i]=seq_det.augment_image(_msk[...,i])
