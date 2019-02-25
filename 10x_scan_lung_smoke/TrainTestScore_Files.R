451df6aaaf963cbce59fb068e31e90df29cdb884
CarolS LabWin10 <ruixiao85@gmail.com>
2019-02-25 15:03:03 -0500

    *Rscript -old_slices

--- 10x_scan_lung_smoke/TrainTestScore_Files.R
+++ 10x_scan_lung_smoke/TrainTestScore_Files.R
@@ -2,23 +2,27 @@ library(ggplot2)
 
 setwd("D:/Cel files/Python/LungNeuralNet/10x_scan_lung_smoke")
 
-sink("score_flat_matrix.csv")
-cat("Profile,Tissue,Repeat,Score\n")
-files=dir(pattern="TanhEluSigmBced1.csv")
+files=dir(pattern="1.csv"); fn="score_1flat" # single
+#files=dir(pattern="6.csv"); fn="score_6flat" # multi
+#files=c(dir(pattern="1.csv"),dir(pattern="6.csv")); fn="score_1+6flat" # multi
+sink(paste0(fn,".csv"))
+cat("Profile,Tissue,Repeat,Score\n");
 for (f in files){
    df=read.csv(f,header=T,stringsAsFactors=F)
-   #cat(paste0(strsplit(f,"_")[[1]][1],max(df$'repeat.'),",",
-   #           max(df[df$'repeat.' %in% c(1,2),]$'dice'),",")) #train
-   #cat(paste0(strsplit(f,"_")[[1]][1],max(df$'repeat.'),",",
-   #           max(df[df$'repeat.' %in% c(1,2),]$'val_dice'),",")) #test
+   fc=gsub(",","",f)
    for (rep in c(1,2)){
-      cat(paste0(gsub("./","",f),",",strsplit(f,"_")[[1]][1],",",rep,",",
-                 max(df[df$'repeat.'==rep,]$'val_dice'),"\n"))
+      if (endsWith(f,"1.csv")) {
+         cat(paste0(gsub("./","",f),",",strsplit(f,"_")[[1]][1],",",rep,",",
+           max(df[df$'repeat.'==rep,]$'val_dice'),"\n")) # single
+      } else {
+        cat(paste0(gsub("./","",fc),",",strsplit(fc,"_")[[1]][1],",",rep,",",
+           max(df[df$'repeat.'==rep,]$'val_acc'),"\n")) # multi
+      }
    }
 }
 sink()
 
-df2=read.csv("score_flat_matrix.csv",header=T,stringsAsFactors=F)
+df2=read.csv(paste0(fn,".csv"),header=T,stringsAsFactors=F)
 df2m<-aggregate(.~Profile+Tissue,FUN=mean,data=df2[,-3])
 df2sd<-aggregate(.~Profile+Tissue,FUN=sd,data=df2[,-3])
 df2<-cbind(df2m,Sd=df2sd[,3])
@@ -37,7 +41,8 @@ df3$profileall=paste(df3$profile.mag,df3$profile.res,df3$profile.arch,
 my_theme<-theme_bw()+theme(axis.text.x = element_text(angle=90,hjust=1,vjust=0.5))
 dodge<-position_dodge(width=0.3)
 require("ggrepel") # text_repel
-pdf("score_flat_facet.pdf",width=7,height=5,pointsize=12)
+
+pdf(paste0(fn,".pdf"),width=7,height=5,pointsize=12) # single
 p=ggplot(df3[df3$profile.mag=="1px/µm",], aes(x=Tissue,y=Score,group=profileall,color=profile.bone))+
    geom_errorbar(aes(ymin=Score-Sd, ymax=Score+Sd),position=dodge,width=0.4)+
    geom_line(aes(linetype=profile.mag),position=dodge)+
@@ -60,3 +65,13 @@ p33+geom_text_repel(aes(label=round(Score,2)),size=3,alpha=0.8,position=dodge)
 dev.off()
 
 
+pdf(paste0(fn,".pdf"),width=7,height=5,pointsize=12) # multi
+p=ggplot(df3[df3$profile.mag=="1px/µm",], aes(x=Tissue,y=Score,group=profileall,color=profile.bone))+
+   geom_errorbar(aes(ymin=Score-Sd, ymax=Score+Sd),position=dodge,width=0.4)+
+   geom_line(aes(linetype=profile.mag),position=dodge)+
+   geom_point(aes(shape=profile.decon),alpha=0.8,position=dodge)+
+   facet_grid(.~profile.act+profile.decon+profile.res+profile.mag)+
+   my_theme+ylim(0.8,1)
+p+geom_text_repel(aes(label=round(Score,2)),size=3,alpha=0.8,position=dodge)
+
+dev.off()
